dec8c855cb7b5b9057d20d338d11adf34902e130
David Mrnustik <david.mrnustik@hotmail.com>
2016-01-10 18:37:32 +0100

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,17 @@ class VisitorsController < ApplicationController
   
   def new
     @owner = Owner.new
-    # render 'visitors/new' # this is what's happened in Rails code library
+    # This is what's happened in Rails code library
+    # render 'visitors/new'
+    
+    # Render without any application layout
+    # render 'visitors/new', layout: false 
+    
+    # Render with specify alternative layout
+    # render 'visitors/new', layout: 'special'
+    
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
   end
   
 end
