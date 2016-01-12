56954afb2662d12787a729bfd5df5d37eaf0eaa0
David Mrnustik <david.mrnustik@hotmail.com>
2016-01-13 00:49:44 +0100

    sending mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,24 +22,24 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: 'smtp.gmail.com',
     port: 587,
     domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
+    authentication: "login",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
   }
-  
-  # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
   
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
